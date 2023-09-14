e9d6a477d95eec5ac77baaaa5ab4ae75dcf94973
azureycy <90859231+azureycy@users.noreply.github.com>
2023-09-13 21:05:03 -0500

    Update family_msa_hmm.sh

--- family_msa_hmm.sh
+++ family_msa_hmm.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+mkdir family_msa
+
 for family_id in APIS{001..093};
 do 
     # multiple sequence alignment of each family
-    mafft --thread 16 --auto $family_id.fasta > $family_id.align.faa;
+    mafft --thread 16 --auto $family_id.fasta > family_msa/$family_id.align.faa;
     # build profile HMM for each family
-    hmmbuild $family_id.hmm $family_id.align.faa;
+    hmmbuild $family_id.hmm family_msa/$family_id.align.faa;
 done
