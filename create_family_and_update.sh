4e402826cb60f3f77db6d7e25a699c2f096375e3
azureycy <90859231+azureycy@users.noreply.github.com>
2023-09-13 20:38:49 -0500

    Update create_family_and_update.sh

--- create_family_and_update.sh
+++ create_family_and_update.sh
@@ -1,21 +1,22 @@
-# run mmseqs2 on GLU
-mmseqs createdb seed_prot_homolog_rmdupseq.fa mmseq_DB/mmseq_DB
+#!/bin/sh
 
+### Generate original batch of APIS protein families ###
+# run mmseqs2 for clustering
+mmseqs createdb seed_prot_homolog_rmdupseq.fa mmseq_DB/mmseq_DB
 mkdir mmseq_clustr_cov0.8
 mmseqs cluster mmseq_DB/mmseq_DB mmseq_clustr_cov0.8/mmseq_clustr tmp --min-seq-id 0.4 -c 0.8
 mmseqs createtsv mmseq_DB/mmseq_DB mmseq_DB/mmseq_DB mmseq_clustr_cov0.8/mmseq_clustr mmseq_clustr.tsv
 
+# etarct sequences of clusters with >= 3 members
 mmseqs createseqfiledb mmseq_DB/mmseq_DB mmseq_clustr_cov0.8/mmseq_clustr mmseq_clustr_cov0.8/mmseq_clustr_seq --min-sequences 3
 mmseqs result2flat mmseq_DB/mmseq_DB mmseq_DB/mmseq_DB mmseq_clustr_cov0.8/mmseq_clustr_seq mmseq_clustr_cov0.8/mmseq_clustr_seq.fasta
 
-# add newly curated APIS protein to existing fmailies
-mmseqs createdb 4_newlycurated_prot_homolog_rmdupseq.fa 4_newlycurated_prot_homolog_rmdupseq_DB
-mmseqs search 4_newlycurated_prot_homolog_rmdupseq_DB ../mmseq_DB newlycurated_outDB tmp --min-seq-id 0.4 -c 0.8
-mmseqs convertalis 4_newlycurated_prot_homolog_rmdupseq_DB ../mmseq_DB newlycurated_outDB newlycurated_outDB.tsv
+### Add newly curated APIS proteins ###
+# compare new APIS proteins with existing APIS families using mmseqs search
+mmseqs createdb newlycurated_prot_homolog_rmdupseq.fa newlycurated_prot_homolog_rmdupseq_DB
+mmseqs search newlycurated_prot_homolog_rmdupseq_DB ../mmseq_DB newlycurated_outDB tmp --min-seq-id 0.4 -c 0.8
+mmseqs convertalis newlycurated_prot_homolog_rmdupseq_DB ../mmseq_DB newlycurated_outDB newlycurated_outDB.tsv
 
-diamond makedb --in ../seed_prot_homolog_rmdupseq.fa -d diamondDB_seed_prot_homolog_rmdupseq
-diamond blastp -d diamondDB_seed_prot_homolog_rmdupseq -q 4_newlycurated_prot_homolog_rmdupseq.fa -o newlycurated_diamond.out --threads 16 -e 1e-10 --id 0.8
-
-# if not hits in mmseqs search/blastp output, then mmseqs cluster to form new family_members
-mmseqs cluster 4_newlycurated_prot_homolog_rmdupseq_DB 4_new_clustering tmp --min-seq-id 0.4 -c 0.8
-mmseqs createtsv 4_newlycurated_prot_homolog_rmdupseq_DB 4_newlycurated_prot_homolog_rmdupseq_DB 4_new_clustering 4_new_clustr.tsv
+# if hits on <70% of members a family, then mmseqs cluster to form new family_members
+mmseqs cluster newlycurated_prot_homolog_rmdupseq_DB new_clustering tmp --min-seq-id 0.4 -c 0.8
+mmseqs createtsv newlycurated_prot_homolog_rmdupseq_DB newlycurated_prot_homolog_rmdupseq_DB new_clustering new_clustr.tsv
