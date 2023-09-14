f649118444ca098e08fbea78376b20c597d15a51
azureycy <90859231+azureycy@users.noreply.github.com>
2023-09-13 20:33:45 -0500

    Update create_family_and_update.sh

--- create_family_and_update.sh
+++ create_family_and_update.sh
@@ -19,22 +19,3 @@ diamond blastp -d diamondDB_seed_prot_homolog_rmdupseq -q 4_newlycurated_prot_ho
 # if not hits in mmseqs search/blastp output, then mmseqs cluster to form new family_members
 mmseqs cluster 4_newlycurated_prot_homolog_rmdupseq_DB 4_new_clustering tmp --min-seq-id 0.4 -c 0.8
 mmseqs createtsv 4_newlycurated_prot_homolog_rmdupseq_DB 4_newlycurated_prot_homolog_rmdupseq_DB 4_new_clustering 4_new_clustr.tsv
-
-# generate mmseq_clustr_count.tsv
-for i in `cut -f1 seed_prot_blast_all_viral.qcov_hsp_perc80.out |sort -u`;do grep $i mmseq_clustr_cov0.8/mmseq_clustr.tsv;done|cut -f1|sort -u > a
-cut -f1 mmseq_clustr_cov0.8/mmseq_clustr.tsv|sort |uniq -c|sed 's/^\s*//'|awk -F" " '{print $2"\t"$1}' > mmseq_clustr_cov0.8/mmseq_clustr_count.tsv
-cut -f1 mmseq_clustr_cov0.8/mmseq_clustr_count.tsv|sort -u > b
-sed 's/$/\tyes/' a > aa    # label family with seed
-diff a b|less|grep ">"|cut -d" " -f2|sed 's/$/\tno/' > bb
-cat aa bb |sort > cc
-join -t $'\t' mmseq_clustr_cov0.8/mmseq_clustr_count.tsv cc > c
-sort -rnk2,2 c > mmseq_clustr_cov0.8/mmseq_clustr_count.tsv
-
-# parse mmseq_clustr_seq.fasta to split seqs of each cluster to each single fasta file
-python seq_each_clustr.py
-
-# generate table for each family's members
-for family in anti_defense_{001..089};
-do for i in `grep ">" ../www/data/each_family_faa/$family.fasta|cut -d" " -f1|sed 's/>//'`;
-do echo -e $family'\t'$i;done;
-done > family_members.tsv
